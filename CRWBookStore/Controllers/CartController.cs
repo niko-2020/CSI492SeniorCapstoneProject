f83e344742a79f6507ad54fef0af504d1f51612a
Mengni Wu <mwu01@student.rtc.edu>
2023-05-02 11:54:15 -0700

    1

---
+++ CRWBookStore/Controllers/CartController.cs
@@ -0,0 +1,22 @@
+﻿using CRWBookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRWBookStore.Controllers
+{
+    public class CartController : Controller
+    {
+        private readonly DataContext _db;
+        public CartController(DataContext db)
+        {
+            _db = db;
+        }
+        /*public async Task<IActionResult> addToCart(int customer_id,int book_id,int count)
+        {
+            customer_id.
+        }*/
+    }
+}
---
+++ CRWBookStore/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using CRWBookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRWBookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+    }
+}
